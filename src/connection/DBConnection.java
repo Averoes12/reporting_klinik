8c7ce62413fd3fcfcb249ca42484b1f3c3bf8cef
Fawwaz Daffa <fawwaz.sayyaf@iconpln.co.id>
2026-05-25 21:04:33 +0700

    chore: edit connection

--- src/connection/DBConnection.java
+++ src/connection/DBConnection.java
@@ -8,14 +8,16 @@ import java.util.logging.Logger;
 
 public class DBConnection {
 //    "jdbc:mysql://bore.pub:23306/klinik?user=root&password=root123"
+//    "jdbc:mysql://localhost:3306/klinik?user=root&password="
+
     private static final Logger LOGGER = Logger.getLogger(DBConnection.class.getName());
-    private static final String HOST = config("db.host", "DB_HOST", "bore.pub");
-    private static final String PORT = config("db.port", "DB_PORT", "23306");
+    private static final String HOST = config("db.host", "DB_HOST", "localhost");
+    private static final String PORT = config("db.port", "DB_PORT", "3306");
     private static final String DATABASE = config("db.name", "DB_NAME", "klinik");
-    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE;
-//            + "?zeroDateTimeBehavior=CONVERT_TO_NULL";
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
+            + "?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private static final String USER = config("db.user", "DB_USER", "root");
-    private static final String PASSWORD = config("db.password", "DB_PASSWORD", "root123");
+    private static final String PASSWORD = config("db.password", "DB_PASSWORD", "");
 
     public Connection connect() {
         try {
